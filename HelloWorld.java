1d08854082de90b6fb54d30a057bc9889cd4956a
Tushar <tush_c@yahoo.com>
2022-01-25 12:57:22 +0530

    Test 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
-public static void main (String [] args) {
-  System.out.println ("Hello World");
+public class HelloWorld {
+    public static void main (String [] args) {
+      System.out.println ("Hello World");
+
+    }
 }
